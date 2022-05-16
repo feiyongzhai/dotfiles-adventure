b4cd6f0ef718232fa1bbe00d685b96dce4bbbb92
yongfei-manjaro <yongfeizhai@qq.com>
2022-05-16 15:11:16 +0800

    解决 active_win_id 为 0x00*** 形式的情况

--- script/cycle-window.sh
+++ script/cycle-window.sh
@@ -8,13 +8,13 @@ win_list=$(wmctrl -x -l | grep -i $win_class\\.$win_class | awk '{print $1}' )
 echo win_list $win_list
 
 # get id of the focused window
-active_win_id=$(xprop -root | grep '^_NET_ACTIVE_W' | awk -F'# 0x' '{print $2}' | awk -F', ' '{print$2$1}')
+active_win_id=$(xprop -root | grep '^_NET_ACTIVE_W' | awk -F'# 0x' '{print $2}' | awk -F', ' '{print$1}')
 #BUG: when a window id is 0x00***(wmctrl -xl output), but xprop -root _NET_ACTIVE_WINDOW return ***(少一个0)
 #这就导致后面的操作不匹配，无法正确识别当前激活的窗口
 echo active_win_id $active_win_id
 
 # get next window to focus on, removing id active
-# 注意下面 sed 命令要用双引号
+# 注意下面 sed 命令要用双引号，而且 .* 这个正则表达式可以解决 active_win_id 开头是 0x00*** 的情况
 switch_to=$(echo $win_list | sed "s/.*$active_win_id//" | awk '{print $1}')
 
 # if the current window is the last in the list ... take the first one
