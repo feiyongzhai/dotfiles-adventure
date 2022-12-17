b0e01c792b45189007ad993f8874b0998e238eda
yongfei-win10 <yongfeizhai@outlook.com>
2022-12-17 19:02:46 +0800

    添加 powershell 的配置

---
+++ win10/profile.ps1
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# 文件位置为：$PROFILE.CurrentUserAllHosts
+
+# z.lua
+Invoke-Expression (& { (lua C:\scoop\apps\z.lua\current\z.lua --init powershell) -join "`n" })
+
+# alias
+Set-Alias of start -Scope Global
+
+# REF: https://www.cnblogs.com/sober-orange/p/powershell-shotkey-windows-terminal.html
+Set-PSReadLineOption -EditMode Emacs #会改变按键行为
+
