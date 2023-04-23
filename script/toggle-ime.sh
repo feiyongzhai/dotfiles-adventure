866ae1d5f38b7738d8ad298d2c9285ea03b46cbb
yongfei-manjaro <yongfeizhai@qq.com>
2023-04-23 23:52:37 +0800

    更新 toggle-ime.sh ，比以前更好用了
    但是还是有点问题：
    xfce-terminal 下不能切换

--- script/toggle-ime.sh
+++ script/toggle-ime.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 CURRENT_WIN=$(xdotool getactivewindow)
-EMACS=$(xdotool search --name 'GNU Emacs')
+EMACS=$(xdotool search --classname --onlyvisible emacs)
 
 if echo $EMACS|grep -q $CURRENT_WIN; then
-    # emacs -Q;
-    xdotool key --delay 100 Ctrl+backslash
+    # xdotool key --delay 300 ctrl+backslash # 效果不好，会出现吞 ctrl 按键，发送失灵的情况
+    xdotool getactivewindow key ctrl+backslash # 稳定，不吞 ctrl
 else
+    # 但是在 terminal 下不可以正常工作，
     fcitx-remote -t
 fi
